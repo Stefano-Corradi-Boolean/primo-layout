39bbf690dab9e212f5913dc07e82a886a9368c29
ugodeughi <stefano.corradi@gmail.com>
2024-05-09 13:11:24 +0200

    fine LC

---
+++ resources/views/home.blade.php
@@ -0,0 +1,16 @@
+
+@extends('layouts.main')
+
+@section('content')
+    <div class="container my-5">
+      <h1>Home</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Dolor perferendis vitae rem quas aliquid minus iure sint amet dignissimos magnam. Laboriosam veritatis, perspiciatis ipsam pariatur doloremque necessitatibus rem nihil exercitationem distinctio quos quasi molestiae quam aperiam, sequi omnis expedita ducimus, nulla debitis recusandae id quidem nostrum nisi quo facere! Quia laboriosam quos tenetur praesentium, impedit dicta nulla beatae a molestiae rerum quisquam. Ex nulla ab unde nisi deleniti quam cumque culpa. Quisquam voluptatem commodi quam reprehenderit expedita tenetur sunt eum fugiat maiores nesciunt sapiente earum cupiditate, praesentium ea aut. A illo, eum possimus dolorum inventore, rerum laboriosam quidem quisquam aperiam quos suscipit, cupiditate architecto maiores tempore deleniti necessitatibus natus? Soluta labore odit aperiam nesciunt. Cupiditate pariatur esse, qui quidem harum veritatis impedit, nesciunt ullam explicabo distinctio cum ex nihil ab eveniet, ut quo reiciendis nobis totam eos nostrum. Natus ab beatae similique at, velit accusamus error quisquam incidunt eaque iusto fugiat repellat, tenetur vero eveniet laboriosam officia! Sed illum vitae eaque dolor debitis quisquam similique rem libero provident modi praesentium aliquid earum omnis alias ullam delectus labore ex nam nemo expedita sequi voluptatem, consequatur enim. Porro harum animi nemo quas, voluptatibus quod eveniet! Culpa quae consequuntur fugiat voluptatum, architecto, delectus, molestias perspiciatis aliquid reprehenderit assumenda porro repellat minima quasi quaerat similique laborum nulla fuga esse. Voluptas veniam, in distinctio placeat maxime amet aliquid vel harum repudiandae esse quam consectetur! Voluptatem tenetur nisi eos rerum quam, quaerat nobis, in id est distinctio cupiditate tempore dolor eligendi quisquam magni velit sed fugiat reiciendis! Laudantium pariatur, voluptatibus dignissimos animi corrupti beatae illo qui eveniet veniam ratione reiciendis totam, rem ad quibusdam quidem ipsum rerum numquam inventore commodi hic fugit, exercitationem assumenda. Quisquam pariatur numquam, corporis quae nostrum voluptatibus cupiditate distinctio deserunt labore iusto sint impedit esse, quo eum doloribus consequatur ipsum, quibusdam debitis.
+      </p>
+
+    </div>
+@endsection
+
+@section('titolo')
+  Home page
+@endsection
---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+  <p class="py-5 text-center bg-black text-white">by UdU</p>
+</footer>
